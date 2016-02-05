b82287db734e004486d573b8c572792011a2a6db
F483 <fabian.barkhau@gmail.com>
2016-02-05 14:06:21 -0500

    added compatibility scripts for dependant projects

---
+++ test_storjnode_compatibility.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export STORJTERMS_RPC_URL="http://127.0.0.1:7000"
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TMP_TEST_DIR=/tmp/storjspec_$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
+git clone https://github.com/storj/storjspec -b master $TMP_TEST_DIR
+cd $TMP_TEST_DIR
+make test_storjnode
+rm -rf $TMP_TEST_DIR
