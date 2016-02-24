675a03cc1836f8bc1bf6adc116ab37bccbe3ad98
F483 <fabian.barkhau@gmail.com>
2016-02-24 20:28:51 +0100

    added dht id and peers tests

--- storjnet/test_dht.py
+++ storjnet/test_dht.py
@@ -3,8 +3,8 @@ import unittest
 import pyjsonrpc
 
 
-# FIXME require more then 1 rpc url to test swarm
-DEFAULT_RPC_URL = "http://127.0.0.1:5000"
+# FIXME test entire swarm not just one node
+DEFAULT_RPC_URL = "http://127.0.0.1:5001"
 STORJNET_RPC_URL = os.environ.get("STORJNET_RPC_URL", DEFAULT_RPC_URL)
 
 
@@ -13,7 +13,16 @@ class TestDhtUserApi(unittest.TestCase):
     def setUp(self):
         self.rpc = pyjsonrpc.HttpClient(url=STORJNET_RPC_URL)
 
-    def test_store(self):
+    def test_id(self):
+        self.assertIsNotNone(self.rpc.dht_id())
+
+    def test_peers(self):
+        peers = self.rpc.dht_peers()
+        print("peers", peers)
+        self.assertIsNotNone(peers)
+        self.assertNotEqual(0, len(peers))
+
+    def test_store(self):  # test put and get
         self.assertTrue(self.rpc.dht_put("testkey", "testvalue"))
         self.assertEqual(self.rpc.dht_get("testkey"), "testvalue")
 
