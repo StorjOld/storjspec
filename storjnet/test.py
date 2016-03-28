9b4ba09d69196c8dd921afa2aff4263474cfe837
F483 <fabian.barkhau@gmail.com>
2016-03-28 21:11:59 +0200

    add stream tests

--- storjnet/test.py
+++ storjnet/test.py
@@ -92,10 +92,9 @@ class TestMessageUserApi(unittest.TestCase):
             self.swarm.append(pyjsonrpc.HttpClient(url=url))
 
     def test_send_receive(self):
+        random.shuffle(self.swarm)
         senders = self.swarm[:len(self.swarm)/2]
         receivers = self.swarm[len(self.swarm)/2:]
-        random.shuffle(senders)
-        random.shuffle(receivers)
         for sender, receiver in zip(senders, receivers):
             message = binascii.hexlify(os.urandom(64))
 
@@ -116,6 +115,7 @@ class TestMessageUserApi(unittest.TestCase):
             self.assertFalse(bool(receiver.message_list()))
 
     def test_ordering(self):
+        random.shuffle(self.swarm)
         sender = self.swarm[0]
         receiver = self.swarm[-1]
 
@@ -151,6 +151,7 @@ class TestMessageUserApi(unittest.TestCase):
         self.assertEqual(messages[0], message)
 
     def test_json(self):
+        random.shuffle(self.swarm)
         sender = self.swarm[0]
         receiver = self.swarm[-1]
         message = {
@@ -226,6 +227,7 @@ class TestPubSubUserApi(unittest.TestCase):
             self.assertEqual(events, [event])
 
     def test_multihop(self):
+        random.shuffle(self.swarm)
         senders = self.swarm[:len(self.swarm)]
         receivers = self.swarm[len(self.swarm):]
         for sender, receiver in zip(senders, receivers):
@@ -247,6 +249,80 @@ class TestPubSubUserApi(unittest.TestCase):
             # check all peers received the event
             events = receiver.pubsub_events(topic)
             self.assertEqual(events, [event])
+
+
+class TestStreamUserApi(unittest.TestCase):
+
+    def setUp(self):
+        self.swarm = []
+        for i in range(SWARMSIZE):
+            url = "http://{0}:{1}".format(USER_HOST, USER_START_PORT + i)
+            self.swarm.append(pyjsonrpc.HttpClient(url=url))
+
+    def test_open(self):
+        random.shuffle(self.swarm)
+        alice = self.swarm[0]
+        bob = self.swarm[-1]
+        streamid = alice.stream_open(bob.dht_id())
+        self.assertIsNotNone(streamid)
+
+    def test_io(self):
+        random.shuffle(self.swarm)
+        alice = self.swarm[0]
+        bob = self.swarm[-1]
+
+        # open stream
+        hexstreamid = alice.stream_open(bob.dht_id())
+        self.assertIsNotNone(hexstreamid)
+
+        # write alice to bob
+        alice_hexdata = binascii.hexlify(os.urandom(32))
+        bytes_written = alice.stream_write(hexstreamid, alice_hexdata)
+        self.assertEqual(bytes_written, 32)
+
+        # write bob to alice
+        bob_hexdata = binascii.hexlify(os.urandom(32))
+        bytes_written = bob.stream_write(hexstreamid, bob_hexdata)
+        self.assertEqual(bytes_written, 32)
+
+        # read alice from bob
+        read_data = alice.stream_read(hexstreamid)
+        self.assertEqual(read_data, bob_hexdata)
+
+        # read bob from alice
+        read_data = bob.stream_read(hexstreamid)
+        self.assertEqual(read_data, alice_hexdata)
+
+    def test_close(self):
+        random.shuffle(self.swarm)
+        alice = self.swarm[0]
+        bob = self.swarm[-1]
+
+        # open stream
+        hexstreamid = alice.stream_open(bob.dht_id())
+        self.assertIsNotNone(hexstreamid)
+
+        # transfer works before
+        written_hexdata = binascii.hexlify(os.urandom(32))
+        bytes_written = alice.stream_write(hexstreamid, written_hexdata)
+        self.assertEqual(bytes_written, 32)
+        read_hexdata = bob.stream_read(hexstreamid)
+        self.assertEqual(read_hexdata, written_hexdata)
+
+        # test close
+        self.assertTrue(alice.stream_close(hexstreamid))
+
+        # write fails on closed stream
+        written_hexdata = binascii.hexlify(os.urandom(32))
+        bytes_written = alice.stream_write(hexstreamid, written_hexdata)
+        self.assertEqual(bytes_written, None)
+
+        # read fails on closed stream
+        read_hexdata = bob.stream_read(hexstreamid)
+        self.assertEqual(read_hexdata, None)
+
+    # TODO test both can open
+    # TODO test both can close
 
 
 if __name__ == "__main__":
